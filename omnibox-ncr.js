7ecb20aed61c8ca09abfa08b6ee077540f325357
Heyo <csxc.me@gmail.com>
2018-06-14 13:31:23 +0800

    change redirect to block

--- omnibox-ncr.js
+++ omnibox-ncr.js
@@ -1,13 +1,5 @@
-// permissions update source: https://www.google.com/supported_domains
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    var requestURL = details.url;
-    // https://regex101.com/r/bcBcDL/3
-    var re = new RegExp('^(http(?:s)?):\/\/www\.google\.(?:(?!com\/)[a-z]+\.?[a-z]+)', 'i');
-    return {
-      redirectUrl: requestURL.replace(re, '$1://www.google.com')
-    };
-  },
-  {urls: ['<all_urls>']},
+  function () { return { cancel: true } },
+  { urls: ["https://www.google.com/searchdomaincheck?format=domain&type=chrome"] },
   ['blocking']
-);
+)
